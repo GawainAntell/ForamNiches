c180033a51ddb1b3ce328e1b5809febd93455510
Gwen Antell <gwenantell@gmail.com>
2019-08-12 11:57:10 +0100

    Retrieve eigenvalues for PC rotation of available environment, then transform species' niche values to PC axes.

--- foram_niche_construction.R
+++ foram_niche_construction.R
@@ -14,6 +14,8 @@ pkgs <- c('sp','raster')
 
 day <- format(as.Date(date(), format="%a %b %d %H:%M:%S %Y"), format='%y%m%d')
 
+
+#############################
 # Read in occurrence and environmental data
 
 occ_all <- read.csv('Data/foram_uniq_occs_latlong190808.csv', stringsAsFactors=FALSE)
@@ -29,12 +31,86 @@ r <- lapply(ann_fls, function(x){
   brik[[1]]
 })
 r <- append(r, raster(max_mo_fl))
-llPrj <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
+env_nms <- vector()
+for (fl in c(ann_fls,max_mo_fl)){
+  env_nm_trunc <- tail(strsplit(fl, 'mean', fixed=TRUE)[[1]], 1)
+  nm_lngth <- nchar(env_nm_trunc)
+  env_nm_trunc <- substr(env_nm_trunc, 9, nm_lngth-4)
+  env_nms <- c(env_nms, env_nm_trunc)
+}
+names(r) <- env_nms
+llPrj <- proj4string(r[[1]])
+
+# Calculate 'available' environment from all occs in a bin
+avlbl <- occ
+avlbl$species <- 'available'
+occ <- rbind(occ, avlbl)
 
 # Extract values at surface along each env axis, for each species
 
-coords <- occ[,c('pal.long','pal.lat')]
-pts <- SpatialPoints(sp_coords, proj4string = CRS(prj))
+
+#############################
+# Retrieve eigenvalues for PC rotation of available environment
+
+avlbl_coords <- avlbl[,c('pal.long','pal.lat')]
+avlbl_pts <- SpatialPoints(avlbl_coords, proj4string = CRS(llPrj))
+avlbl_vals <- lapply(r, raster::extract, y=avlbl_pts)
+sum_vals <- function(x, fn, na.rm=TRUE){
+  if (class(x)=='matrix'){
+    out <- apply(x, 1, fn, na.rm=na.rm)
+  } 
+  if (class(x)=='numeric'){
+    out <- x
+  } 
+  return(out)
+}
+avlbl_cln <- lapply(1:3, function(i){
+  f <- list(min, median, max)[[i]]
+  avlbl_l <- lapply(avlbl_vals, sum_vals, fn=f)
+  avlbl_m <- do.call(cbind, avlbl_l)
+  
+  # some points lack env data; value will be -Inf
+  avlbl_m[is.infinite(avlbl_m)] <- NA
+  avlbl_df <- data.frame(na.omit(avlbl_m))
+  avlbl_df$stat <- c('min','med','max')[i]
+  avlbl_df
+}
+)
+avlbl_by_stat <- do.call(rbind, avlbl_cln)
+colnames(avlbl_by_stat) <- c(env_nms,'stat')
+
+# mean ann temp and max monthly temp are too collinear (cor=.8)
+med_rows <- avlbl_by_stat$stat=='med'
+avlbl_med <- avlbl_by_stat[med_rows,-6]
+ann_t_col <- which(env_nms=='ann_temp_ym_dpth')
+avlbl_med <- avlbl_med[,-ann_t_col]
+pc_avlbl <- prcomp(avlbl_med, center=TRUE, scale=TRUE)
+pc_rot <- pc_avlbl$rotation
+
+
+#############################
+# Extract min, med, max values along each env axis, for each species
+
+env_extract <- function(sp, env, dat, coord_cols, sp_col, prj){
+  sp_bool <- dat[,sp_col] == sp
+  sp_coords <- dat[sp_bool, coord_cols]
+  sp_pts <- SpatialPoints(sp_coords, proj4string = CRS(prj))
+  
+  # If extracted from Brick, values are put in a matrix
+  # Rows = points of extraction, columns = layers
+  vals <- extract(env, sp_pts)
+  mn <- min(vals, na.rm = TRUE)
+  med <- median(vals, na.rm = TRUE)
+  mx <- max(vals, na.rm = TRUE)
+  
+  # Extract the name of the environmental variable
+  env_nm <- env@file@name
+  env_nm_trunc <- tail(strsplit(env_nm, '\\', fixed=TRUE)[[1]], 1)
+  nm_lngth <- nchar(env_nm_trunc)
+  env_nm_trunc <- substr(env_nm_trunc, 8, nm_lngth-4)
+  
+  c(sp, env_nm_trunc, mn, med, mx)
+}
 
 ncores <- detectCores() - 1
 registerDoParallel(ncores) 
@@ -50,6 +126,7 @@ nich_long <- data.frame(nich_m, stringsAsFactors=FALSE)
 #colnames(nich_long) <- c('sp', 'axis', sums)
 nich <- spread(nich_long, key=axis, value=value)
 
+
 #############################
 # PCA of niche position
 
@@ -60,13 +137,35 @@ too_scarce <- names( which(spp_freq<6) )
 spp <- setdiff(spp, too_scarce)
 n_spp <- length(spp)
 
-# correlation between max mont and ann temp is VERY high (.89):
-# exclude annual temp in PCA
-cor(nich[,3:7])
-ann_col <- which(colnames(nich)=='ann_temp_ym_dpth')
-nich <- nich[,-ann_col]
+# format fully-birfurcating tree from Aze et al. 2011
+tr_raw <- read.csv('Data/Aze_et_al_2011_bifurcating_tree_data.csv', stringsAsFactors=FALSE)
+tr_raw$Species.name <- gsub('Globigerinoides sacculifer', 'Trilobatus sacculifer', tr_raw$Species.name)
+tr_raw$Species.name <- gsub('Globigerinoides trilobus', 'Trilobatus trilobus', tr_raw$Species.name)
+tr_paleo <- with(tr_raw, 
+                 as.paleoPhylo(Species.code, Ancestor.code, Start.date, End.date)
+                 )
+tr <- buildApe(tr_paleo)
+spp_codes <- tr$tip.label
+row_ordr <- match(spp_codes, tr_raw$Species.code)
+tr$tip.label <- tr_raw$Species.name[row_ordr]
 
-row.names(nich) <- nich$sp
-nich <- nich[,-1]
-Pc <- prcomp(nich, center=TRUE, scale=TRUE)
+# 8 species are not present in the phylogeny, mostly because microporiferate
+lost_spp <- setdiff(spp, tr$tip.label)
+lost_spp <- setdiff(lost_spp, 'available')
+rows2toss <- nich_by_stat$sp %in% lost_spp
+nich_by_stat <- nich_by_stat[!rows2toss,]
+
+# for (stat in sums){
+stat <- 'max'
+
+  sbset <- nich_by_stat$stat==stat
+  nich_part <- nich_by_stat[sbset,]
+  row_nms <- nich_part$sp
+  nich_part <- nich_part[,-(1:2)]
+  nich_part_scl <- apply(nich_part, 2, scale.default)
+  row.names(nich_part_scl) <- row_nms
   
+  # rotate to PCA axes of available modern environment
+  nich_part_pc <- nich_part_scl*pc_rot
+# }
+
