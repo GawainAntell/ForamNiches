5879efc972934c6bfe64235e0eb9f9ed63bb69ae
Gwen Antell <gwenantell@gmail.com>
2019-09-10 11:13:48 +0100

    Add depth ranges for more species. Remove data from shallow sites. Allow time resolution to be specified.

--- foram_occ_data_prep.R
+++ foram_occ_data_prep.R
@@ -5,6 +5,8 @@ library(foreach)
 library(iterators)
 library(doParallel)
 
+tRes <- 8
+
 # Read in occurrence data
 occ <- read.csv('Data/Foram_data_800ka_IF190709.csv', stringsAsFactors = FALSE)
 
@@ -34,18 +36,45 @@ penult <- nchar(abund) - 1
 dpthTbl$MaxAbund <- substr(abund, 1, penult)
 dpthTbl$MaxAbund <- as.numeric(dpthTbl$MaxAbund)
 
-# Add G. trilobus data from Loncaric et al. 2006
+# Add G trilobus data from Loncaric et al. 2006
 trilobus <- c('Globigerinoides.trilobus',
               NA,208,17.4,NA,7.0,NA,'Surface',NA)
 dpthTbl <- rbind(dpthTbl,trilobus)
 
+# Add G (H) theyeri data from Schiebel et al. 2004
+# found only from 40-60 m, very rare
+theyeri <- c('Hirsutella.theyeri',
+             NA,0.2,50,NA,15,NA,'Surface',NA)
+dpthTbl <- rbind(dpthTbl,theyeri)
+
+# Add depth ranges from Schiebel and Hemleben 2017
+
+# B digitata is subsurface
+digitata <- grep('Beella.digitata',dpthTbl$Species)
+dpthTbl$DepthHabitat[digitata] <- 'Subsurface'
+dpthTbl$ALD[digitata] <- 150
+dpthTbl$VD[digitata] <- 75
+
+# S dehiscens is sub-thermocline, subsurface
+dehiscens <- c('Sphaeroidinella.dehiscens',
+               NA,NA,150,NA,75,NA,'Subsurface',NA)
+
+# G ungulata has same distribution as G menardii; 
+# density up to 2 per cubic m
+ungulata <- c('Globorotalia.ungulata',
+         NA,NA,46.2,NA,25.2,NA,'Surface',NA)
+dpthTbl <- rbind(dpthTbl, dehiscens, ungulata)
+
 # Add depth ranges from Watkins et al. 1996
 
 # G menardii ranges from 20-60m, ALD of 25 read off fig 6c
+# Schiebel et al. 2004 called menardii an upwelling indicator sp
+# and report densities that give ALD=58.6 and Vd=21.9 for one leg,
+# ALD=46.2 and VD=25.2 for another leg (with higher concentrations)
 menardii <- grep('Globorotalia.menardii',dpthTbl$Species)
-dpthTbl$ALD[menardii] <- 25
-dpthTbl$VD[menardii] <- 10
-dpthTbl$MaxAbund[menardii] <- 3
+dpthTbl$ALD[menardii] <- 46.2
+dpthTbl$VD[menardii] <- 25.2
+dpthTbl$MaxAbund[menardii] <- 51
 dpthTbl$DepthHabitat[menardii] <- 'Surface'
 
 # G tumida ranges lives in upper 80m, ALD of 50 read off fig 4b
@@ -59,11 +88,21 @@ conglob <- c('Globigerinoides.conglobatus',
 # G hexagonus is sub-thermocline, 100-200m
 hexag <- c('Globorotaloides.hexagonus',
            NA,2,150,NA,75,NA,'Subsurface',NA)
-dpthTbl <- rbind(dpthTbl,tumida,conglob,hexag)
+
+# G adamsi is sub-termocline, 100-200 m
+# Schiebel et al. 2004 sampled adamsi mainly 40-100m,
+# but didn't sample below 100 m
+adamsi <- c('Globigerinella.adamsi',
+            NA,2,125,NA,75,NA,'Surface.subsurface',NA)
+
+# G conglomerata is subsurface, 60-100m, ALD ca 80 (fig 4a)
+# Schiebel et al. 2004: G conglomerata is surface, ALD=42.5, VD=30.8
+glom <- c('Globoquadrina.conglomerata',
+          NA,2.4,42.5,NA,30.8,NA,'Surface.subsurface',NA)
+
+dpthTbl <- rbind(dpthTbl,tumida,conglob,hexag,adamsi,glom)
 
 # Synonymize according to Microtax for congruence with occurrence database
-# Both synonyms of digitata (Beella and Hastigerinella) are present in depth dataset
-# use only Beella to match occurrences of the species
 dpthTbl$Species <- gsub('Globorotalia.scitula','Hirsutella.scitula',dpthTbl$Species)
 dpthTbl$Species <- gsub('Globorotalia.inflata','Globoconella.inflata',dpthTbl$Species)
 dpthTbl$Species <- gsub('Globorotalia.hirsuta','Hirsutella.hirsuta',dpthTbl$Species)
@@ -96,8 +135,8 @@ occ <- occ[,!cols2toss]
 ##########################################################
 # bin to 4 ky intervals to match GCM data
 
-brk <- seq(0, 796, by=4)
-bins <- data.frame(t=brk, b=brk+4, mid=brk+2)
+brk <- seq(0, 800-tRes, by=tRes)
+bins <- data.frame(t=brk, b=brk+tRes, mid=brk+tRes/2)
 
 # age 'zero' = 1950, and some observations are more recent (i.e. negative age)
 bins$t[1] <- -0.1
@@ -108,6 +147,10 @@ for (r in 1:nrow(occ)){
   aBin <- bins$mid[bin]
   occ$bin[r] <- aBin
 }
+
+# No breeding populations at shallow depths
+shall <- which(occ$water.depth < 150)
+occ <- occ[-shall,]
 
 # Rasterize to the resolution of GCM data
 
@@ -162,6 +205,6 @@ pt2-pt1
 fin <- do.call('rbind', stage_dat_list)
 
 day <- format(as.Date(date(), format="%a %b %d %H:%M:%S %Y"), format='%y%m%d')
-df_name <- paste('Data/foram_uniq_occs_latlong', day, '.csv', sep='')
+df_name <- paste('Data/foram_uniq_occs_latlong_',tRes, 'ka', day, '.csv', sep='')
 write.csv(fin, file=df_name, row.names=FALSE)
 
