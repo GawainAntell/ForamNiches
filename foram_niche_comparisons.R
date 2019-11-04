b30ed1cb235bb63839443b5301e217e08b847ee9
Gwen Antell <gwenantell@gmail.com>
2019-11-04 17:39:56 +0000

    Loop niche overlap calculations over all spp and bins

--- foram_niche_comparisons.R
+++ foram_niche_comparisons.R
@@ -9,9 +9,10 @@ bins <- unique(dat$bin)
 # for theyeri and adamsi, niche is quantified in only 1 time bin
 # conglomeratea and dehiscens occur in only 1 set of consecutive bins
 # humilis occurs in 2 sets of consecutive bins
+# Globorotalia ungulata doesn't seem to have >5 occs in any bin
 noGood <- c('Hirsutella theyeri','Globigerinella adamsi',
             'Globoquadrina conglomerata','Sphaeroidinella dehiscens',
-            'Turborotalita humilis')
+            'Globorotalia ungulata','Turborotalita humilis')
 toss <- which(dat$species %in% noGood)
 dat <- dat[-toss,]
 
@@ -65,25 +66,76 @@ library(ecospat)
 pcDat <- read.csv('Data/foram_uniq_occs_latlong_8ka_wEnv_190919.csv',stringsAsFactors=FALSE)
 pcDat <- pcDat[,c('species','bin','cell_number','centroid_long','centroid_lat','pc1','pc2','pc3')]
 
+# if skipping to this section without running the sections above:
+bins <- unique(pcDat$bin)
+spp <- unique(pcDat$species)
+noGood <- c('Hirsutella theyeri','Globigerinella adamsi',
+            'Globoquadrina conglomerata','Sphaeroidinella dehiscens',
+            'Globorotalia ungulata','Turborotalita humilis')
+toss <- which(spp %in% noGood)
+spp <- spp[-toss]
+
+# Subset occurrences such that eas sp has >5 occs per bin
+saveRows <- function(sp, bin){
+  spRows <- which(pcDat$species==sp & pcDat$bin==bin)
+  if (length(spRows)>5){
+    spRows
+  }
+}  
+keepRowsL <- sapply(spp, function(x){
+  sapply(bins, function(b)saveRows(sp=x, bin=b))
+} 
+)
+keepRows <- unlist(keepRowsL)
+pcDat <- pcDat[keepRows,]
+  
 # Resolution of the gridding of the climate space
 R <- 100
 
-glob <- pcDat[, c('pc1','pc2')]
-glob1bool <- pcDat$bin==bins[1]
-glob1 <- pcDat[glob1bool,c('pc1','pc2')]
-glob2bool <- pcDat$bin==bins[2]
-glob2 <- pcDat[glob2bool,c('pc1','pc2')]
-sp1rows <- which(pcDat$species==spp[2] & pcDat$bin==bins[1])
-sp1 <- pcDat[sp1rows, c('pc1','pc2')]
-sp2rows <- which(pcDat$species==spp[2] & pcDat$bin==bins[2])
-sp2 <- pcDat[sp2rows, c('pc1','pc2')]
+overlapD <- function(b1, b2, sp){
+  glob <- pcDat[, c('pc1','pc2')]
+  glob1bool <- pcDat$bin==b1
+  glob1 <- glob[glob1bool,]
+  glob2bool <- pcDat$bin==b2
+  glob2 <- glob[glob2bool,]
+  sp1rows <- which(pcDat$species==sp & pcDat$bin==b1)
+  sp1 <- glob[sp1rows,]
+  sp2rows <- which(pcDat$species==sp & pcDat$bin==b2)
+  sp2 <- glob[sp2rows,]
+  
+  # for each species at time i and i+1
+  z1 <- tryCatch(
+    ecospat.grid.clim.dyn(glob, glob1, sp1, R, th.sp=0.05, th.env=0.05),
+    error = function(err){ NA }
+  ) 
+  z2 <- tryCatch(
+    ecospat.grid.clim.dyn(glob, glob2, sp2, R, th.sp=0.05, th.env=0.05),
+    error = function(err){ NA }
+  ) 
+  # plot(z1$z) # raster of niche in PC space
+  
+  if (any(is.na(list(z1, z2)))){
+    NA
+  } else {
+    ovrlp <- ecospat.niche.overlap(z1, z2, cor=FALSE)
+    ovrlp$D
+  }
+}
 
-# for each species at time i and i+1
-z <- ecospat.grid.clim.dyn(glob, glob1, sp1, R, th.sp=0.05, th.env=0.05)
-# plot(z$z) # raster of niche in PC space
-z2 <- ecospat.grid.clim.dyn(glob, glob2, sp2, R, th.sp=0.05, th.env=0.05)
-plot(z$z)
-plot(z2$z)
+bPairs <- cbind(bins[-1], bins[-length(bins)])
+Dlist <- lapply(spp, function(s){
+  apply(bPairs, 1, function(x){
+    overlapD(b1=x[1], b2=x[2], sp=s)
+  })
+})
+dDf <- do.call(cbind, Dlist)
+row.names(dDf) <- bins[-1]
+colnames(dDf) <- spp
 
-ovrlp <- ecospat.niche.overlap(z, z2, cor=FALSE)
-ovrlp$D
+day <- format(as.Date(date(), format="%a %b %d %H:%M:%S %Y"), format='%y%m%d')
+dfNm <- paste0('Data/foram_niche_overlap_D_',day,'.csv')
+write.csv(dDf, dfNm)
+
+####################################################################
+# Velocity of climate change
+
