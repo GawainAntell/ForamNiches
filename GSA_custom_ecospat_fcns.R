6eaf6c21ee0510c18eb514d584f55123aa6bd75c
Gwen Antell <gwenantell@gmail.com>
2020-01-27 17:17:24 +0000

    Throw out unnecessary ecospat code and related comments. Pare down to the niche functions that are actually needed.

--- GSA_custom_ecospat_fcns.R
+++ GSA_custom_ecospat_fcns.R
@@ -1,12 +1,9 @@
+library(pracma)
 
-GSA.grid.clim.dyn <- 
-  function (glob, glob1, sp, R, th.sp = 0, th.env = 0, geomask = NULL, h.method="nrd0") {
-    glob <- as.matrix(glob)
-    glob1 <- as.matrix(glob1)
+kdeNiche <- 
+  function (sp, xmax, xmin, R, h.method="nrd0") {
     sp <- as.matrix(sp)
     l <- list()
-    xmax <- max(glob[, 1])
-    xmin <- min(glob[, 1])
     x <- seq(from = xmin, to = xmax, length.out = R)
     # Arguments 'from', 'to', 'R', and 'cut' don't affect bandwidth for nrd0, SJ-ste, or ucv
     # but they do affect the roughness of the density plot - smoother (better) with defaults
@@ -27,56 +24,14 @@ GSA.grid.clim.dyn <-
                        n = R
                   #     cut = 0
                        )
-    glob1.dens <- density(glob1[, 1], 
-                          kernel = "gaussian", 
-                          bw=h.method,
-                          from = xmin, 
-                          to = xmax, 
-                          n = R
-                  #        cut = 0
-                          )
-    # Original ecospat code re-scales the distribution by the max density value.
-    # The reason why is unclear, and this step does very slightly change Schoener's D.
- #   z <- z$y # * nrow(sp)/sum(z$y)
-    Z <- glob1.dens$y # * nrow(glob)/sum(glob1.dens$y)
-    
-    # Original ecospat code truncated the KDE by the quantile supplied.
-    # This functionality is not used here, and setting quantile = 0 leads to bad behaviour,
-    # so this section is commented out.
-      # glob1r <- sapply(glob1, findInterval, glob1.dens$x)
-      # th.env <- quantile(glob1.dens$y[glob1r], th.env)
-      # glob1rm <- which(Z < th.env)
-      # spr <- sapply(sp, findInterval, z$x)
-      # th.sp <- quantile(z$y[spr], th.sp)
-      # sprm <- which(z < th.sp)
-      # z[sprm] <- 0
-      # Z[glob1rm] <- 0
-    
-    # z.uncor <- z/max(z)
-    z.cor <- z$y/Z
-    z.cor[is.na(z.cor)] <- 0
-    z.cor[z.cor == "Inf"] <- 0
-    # z.cor <- z.cor/max(z.cor)
-    
-    # These outputs are commented out because they aren't needed for later analysis.
-#    w <- z.uncor
-#    w[w > 0] <- 1
-#    l$x <- x
     l$z <- z$y
-#    l$z.uncor <- z.uncor
-    l$z.cor <- z.cor
-    l$Z <- Z
-#    l$glob <- glob
-#    l$glob1 <- glob1
-#    l$sp <- sp
-#    l$w <- w
     
     # Add Holland 1995 niche summary metrics to output:
     # peak abundance, preferred environment, & tolerance
     l$pa <- max(z$y)
     pe.pos <- which.max(z$y)
     l$pe <- x[pe.pos]
-    l$t <- sd(z$x)
+    l$t <- sd(sp)
     
     # To approximate the KDE as a function, one also needs x-values:
     l$x <- z$x
@@ -84,84 +39,19 @@ GSA.grid.clim.dyn <-
     return(l)
   }
 
-GSA.ecospat.niche.overlap <- function (z1, z2, cor) {
-  if (cor == FALSE) {
-    # The original code scales density estimates by the max value,
-    # then takes sum as if it were a discrete mass function.
-#    p1 <- as.matrix(z1$z.uncor)/sum(as.matrix(z1$z.uncor))
-#    p2 <- as.matrix(z2$z.uncor)/sum(as.matrix(z2$z.uncor))
-    # However, the KDE approximates a continuous density function.
-    # So, find that function and integrate it.
-    # integrating the density distribution as a continuous function:
-    fun1 <- approxfun(z1$x, z1$z)
-    fun2 <- approxfun(z2$x, z2$z)
-    # Package warning:
-    # 'The value returned by approxfun contains references to the code in the current version of R: 
-    # it is not intended to be saved and loaded into a different R session. This is safer for R >= 3.0.0.'
-    # Also note: since the functions integrate to < 1, the minimum achievable D value is > 0.
-  }
-  if (cor == TRUE) {
-    fun1 <- approxfun(z1$x, z1$z.cor)
-    fun2 <- approxfun(z2$x, z2$z.cor)
-#    p1 <- as.matrix(z1$z.cor)/sum(as.matrix(z1$z.cor))
-#    p2 <- as.matrix(z2$z.cor)/sum(as.matrix(z2$z.cor))
-  }
-  
-  difFun <- function(x) abs(fun1(x) - fun2(x))
-  xmin <- min(z1$x)
-  xmax <- max(z1$x)
-  int <- integrate(difFun, xmin, xmax)
-  D <- 1 - int$value * 0.5
-  return(D)
-}
-
-# TODO find out why all of the below functions have 'extremely bad integrand behaviour'
-# note that subdivisions should be > R
-
-d1 <- function (z1, z2) {
-  fun1 <- approxfun(z1$x, z1$z)
-  fun2 <- approxfun(z2$x, z2$z)
-  difFun <- function(x) abs(fun1(x) - fun2(x))
-  a <- z1$x[2] #max(min(z1$x), min(z2$x))
-  b <- z1$x[99] #min(max(z1$x), max(z2$x))
-  int <- integrate(difFun, a, b, subdivisions = 200)
-  1 - int$value * 0.5
-}
-
-d2 <- function (z1, z2) {
-  if (! identical(z1$x, z2$x)){stop('different axes')}
-  diff <- abs(z1$z - z2$z)
-  difFun <- approxfun(z1$x, diff)
-  a <- z1$x[2] #max(min(z1$x), min(z2$x))
-  b <- z1$x[99] #min(max(z1$x), max(z2$x))
-  int <- integrate(difFun, a, b, subdivisions = 200)
-  1 - int$value * 0.5
-}
-
-# Bhattacharyya distance
-bc <- function (z1, z2) {
-  fun1 <- approxfun(z1$x, z1$z)
-  fun2 <- approxfun(z2$x, z2$z)
-  intgrnd <- function(x) sqrt(fun1(x) * fun2(x))
-  a <- z1$x[2] #max(min(z1$x), min(z2$x))
-  b <- z1$x[99] #min(max(z1$x), max(z2$x))
-  int <- integrate(intgrnd, a, b, subdivisions = 200)
-  int$value
-}
-
 # Hellinger's H
-hinv <- function (z1, z2) {
+hell <- function (z1, z2) {
   fun1 <- approxfun(z1$x, z1$z)
   fun2 <- approxfun(z2$x, z2$z)
   intgrnd <- function(x) sqrt(fun1(x) * fun2(x))
-  a <- z1$x[2] #max(min(z1$x), min(z2$x))
-  b <- z1$x[99] #min(max(z1$x), max(z2$x))
-  int <- integrate(intgrnd, a, b, subdivisions = 200)
-  h <- sqrt(1 - int$value)
-  1 - h
+  if (! identical(z1$x, z2$x)){warning('different x axes')}
+  a <- max(min(z1$x), min(z2$x))
+  b <- min(max(z1$x), max(z2$x))
+  int <- integral(intgrnd, a, b)
+  h <- sqrt(1 - int)
+  h
 }
 
-library(pracma)
 sumErr <- function(z1, z2){
   xmin <- min(z1$x)
   xmax <- max(z1$x)
