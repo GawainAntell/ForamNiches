c4e1403dce11e4edc7def70d20f2d28a31f58011
Gwen Antell <gwenantell@gmail.com>
2020-02-06 14:42:50 +0000

    Generalise MISE calculations for multiple bias fcns

--- kde_bias_correction_attempts.R
+++ kde_bias_correction_attempts.R
@@ -1,4 +1,5 @@
 library(pracma)
+library(GoFKernel)
 
 # Empirical data ----------------------------------------------------------
 
@@ -77,63 +78,94 @@ sp <-
 
 # Simulate data -----------------------------------------------------------
 
-# in Barmi & Simonoff 2000:
-# true Chi-square distribution is k=2 or 12; n = 50 or 200
-nVals <- c(50, 200)
-kVals <- c(2, 12)
-hMeths <- c('nrd0','SJ-ste')
-
+    
 # calculate MISE for chi-sq distribution with given n and df
-simBS <- function(n, k, hMeth){
-  w <- function(x){ x }
+# based on data drawn with known bias, w
+miser <- function(x, n, k, hMeth, nbreak, w){
   
-  # when w(x)=x, then g is a chi-sq density of k+2
-  X <- rchisq(n=n, df=k+2)
+  muHat <- n * sum( w(x)^-1 )^-1
   
-  uhat <- n * sum( w(X)^-1 )^-1
-  Y <- sapply(X, function(x){
-    integrate(w, lower = 0, upper = x)$value
+  test <- c(is.infinite(w(0)), is.na(w(0)))
+  if (any(test)){
+    lwr <- exp(-30)
+  } else {
+    lwr <- 0
+  }
+  Y <- sapply(x, function(u){
+    integral(w, xmin = lwr, xmax = u) 
   })
   
   # kernel density estimation on transformed data
   # note that the authors used local quadratic likelihood instead
-  kdeTrans <- density(Y, kernel='gaussian', bw=hMeth, cut=0)
+  kdeTrans <- density(Y, kernel='gaussian', bw=hMeth, cut=0, n=nbreak)
   
   # scale to integrate to one
-  kdeTrans$y <- uhat * kdeTrans$y
+  kdeTrans$y <- muHat * kdeTrans$y
   
-  # back-transform from the y=W(x) argument to x
+  # Back-transform from the y=W(x) argument to x.
+  # Build custom cdf function, since 
+  # inverse() does not play well with ecdf (step fcn)
+  wcdf <- function(u){
+    integral(w, xmin = lwr, xmax = u) 
+  }
+  wInv <- inverse(wcdf, lower=lwr)
+  
   kdeTrans$xTrans <- kdeTrans$x
-  kdeTrans$x <- sqrt(2 * kdeTrans$x)
-  # plot(kde)
+  # this is the bottleneck step:
+  kdeTrans$x <- sapply(kdeTrans$x, wInv)
+  # plot(kdeTrans)
   
   # calculate MISE (minimum integrated squared error)
-  a <- min(X)
-  b <- max(X)
-  fhat <- approxfun(kdeTrans$x, kdeTrans$y)
-  se <- function(x) (fhat(x) - dchisq(x, df=k))^2
-  mise <- integral(se, a, b)
   
-  # kernel density estimation after Jones 1991
+  # if w(x) is undefined at 0, then min(X) can be larger
+  # (albeit only slightly) than values at which kernel can estimate 
+  a <- min(kdeTrans$x) 
+  b <- max(x)
+  fHatT <- approxfun(kdeTrans$x, kdeTrans$y)
+  seTrans <- function(u) (fHatT(u) - dchisq(u, df=k))^2
+  miseTrans <- integral(seTrans, a, b)
   
-  # calculate  weights
-  wts <- 1/w(X)
+  # weighted kernel density estimation after Jones 1991
+  wts <- 1/w(x)
   wts <- wts/sum(wts)
-  kdeJ <- density(X, kernel='gaussian', bw=hMeth, cut=0,
+  kdeJ <- density(x, kernel='gaussian', bw=hMeth, cut=0, n=nbreak,
                   weights=wts)
   fhatJ <- approxfun(kdeJ$x, kdeJ$y)
   seJ <- function(x) (fhatJ(x) - dchisq(x, df=k))^2
   miseJ <- integral(seJ, a, b)
   
-  c(miseJ, mise)
+  c(miseJ, miseTrans)
 }
+
+# in Barmi & Simonoff 2000: n = 50 or 200
+# for w=x, true Chi-square distribution is k=2 or 12
+# for w=1/x, k=3 or 16
+nVals <- c(50, 200)
+kVals <- c(3, 16)
+  # c(2, 12)
+hMeths <- c('nrd0','SJ-ste')
+w <- function(x){ 
+  1/x 
+#   x # classical length bias
+  }
+nbreak <- 2^8 # number of kernel estimation points
+# using 2^9 gives oly 1x10^-6 better mise for one example
+
+# for TESTING ONLY
+# n <- 50; k <- 3; hMeth <- 'nrd0'
 
 # recreate table 1 from Barmi & Simonoff 2000
 tab1 <- data.frame(matrix(ncol=5, nrow=0))
 for (n in nVals){
   for (k in kVals){
     for (hMeth in hMeths){
-      mises <- replicate(500, simBS(n,k,hMeth))
+      # when w(x)=x, then g is a chi-sq density of k+2
+      # when w(x)=1/x, then chi-sq has k-2
+      x <- rchisq(n=n, df=(k-2))
+      # rchisq(n=n, df=(k-2))
+      
+      # original study used 500 replicates but calculation is slow
+      mises <- replicate(100, miser(x,n,k,hMeth,nbreak,w))
       mise <- colMeans(t(mises))
       newRow <- data.frame(hMeth, n, k, t(mise))
       tab1 <- rbind(tab1, newRow)
