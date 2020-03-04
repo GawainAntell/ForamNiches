2fc3cb58301ff3d0588306caba6e2669d747cfdf
Gwen Antell <gwenantell@gmail.com>
2020-03-04 15:11:01 +0000

    Use Borrajo et al. 2017 bandwidth selection methods for weighted KDE

--- GSA_custom_ecospat_fcns.R
+++ GSA_custom_ecospat_fcns.R
@@ -96,8 +96,6 @@ density.reflected <- function (x, lower = -Inf, upper = Inf, weights = NULL, ...
 # and one should NOT provide the arguments to/from or lower/upper.
 # TODO Add tweak to uniroot function within inverse, for badly behaved empirical data
 transformEst <- function(x, w, reflect = FALSE, a=NULL, b=NULL, ...){
-  argmts <- list(...)
-  
   if (is.null(a)){
     test <- c(is.infinite(w(0)), is.na(w(0)))
     if (any(test)){
@@ -149,10 +147,70 @@ transformEst <- function(x, w, reflect = FALSE, a=NULL, b=NULL, ...){
   append(list(f=f, lower=lwr, upper=upr), kde)
 }
 
+# Borrajo et al. 2017 bandwidth estimation for biased data:
+# rule of thumb or bootstrap method
+strappy <- function(Y, w, method='brt'){
+  if (! method %in% c('brt','rt')){
+    stop('method must be brt or rt')
+  } 
+  
+  # estimate of mu sub omega
+  n <- length(Y)
+  mu <- (sum(1/w(Y))/n)^-1
+  
+  # estimate of sigma-sq sub omega
+  term1 <- sum(w(Y))/n
+  sgma <- sqrt(mu*term1 - mu^2)
+  
+  # estimate of c sub omega
+  cHat <- mu * 1/n * sum(1/w(Y)^2)
+  
+  # estimate rule-of-thumb bandwidth
+  K2u <- function(u){
+    dnorm(u)^2
+  }
+  Rk <- integral(K2u, -Inf, Inf)
+  u2k <- function(u){
+    u^2 * dnorm(u)
+  }
+  mu2K <- integral(u2k, -Inf, Inf) # 1
+  rtNum <- Rk * mu * cHat * 8 * sqrt(pi)
+  rtDenom <- n * mu2K * 3
+  rt <- (rtNum/rtDenom)^(1/5) * sgma
+  
+  if (method=='rt'){
+    return(rt)
+  }
+  
+  if (method=='brt'){
+    # bootstrap estimate of bandwidth, 
+    # with rule-of-thumb for pilot
+    
+    wtsUnscld <- 1/w(Y)
+    wts <- wtsUnscld/sum(wtsUnscld)
+    fgDens <- density(Y, bw=rt, kernel='gaussian', weights=wts)
+    fg <- splinefun(fgDens$x, fgDens$y)
+    
+    # take 2nd deriv of smooth f estimate, square it, and integrate
+    f2g_y <- fg(fgDens$x, deriv=2)
+    f2g <- approxfun(fgDens$x, f2g_y)
+    f2g_sq <- function(u){
+      f2g(u)^2
+    } 
+    a <- min(fgDens$x)
+    b <- max(fgDens$x)
+    Rfg <- integral(f2g_sq, a, b)
+    
+    num <- Rk * mu * cHat
+    denom <- n * mu2K * Rfg
+    brt <- (num/denom)^(1/5)
+    return(brt)
+  }
+}
+
 # weighted kernel density estimation after Jones 1991
-# TODO
-# Use Borrajo's rule of thumb and 2 bootstrap bandwidth estimators
-JonesEst <- function(x, w, reflect=FALSE, a=NULL, b=NULL, ...){
+# bw argument can be rt, brt, or a numeric width to use
+JonesEst <- function(x, w, bw='brt', reflect=FALSE, a=NULL, b=NULL, ...){
   wts <- 1/w(x)
   wts <- wts/sum(wts)
   
@@ -163,10 +221,20 @@ JonesEst <- function(x, w, reflect=FALSE, a=NULL, b=NULL, ...){
     b <- max(x)
   }
   
-  if (reflect){
-    kde <- density.reflected(x, lower = a, upper = b, weights = wts, ...)
+  # select bandwidth
+  if (is.numeric(bw)){
+    h <- argmts$bw
   } else {
-    kde <- density(x, from = a, to = b, weights = wts, ...)
+    if (! bw %in% c('brt','rt')){
+      stop('bw method must be brt or rt')
+    }
+    h <- strappy(x, w, method = bw)
+  }
+  
+  if (reflect){
+    kde <- density.reflected(x, bw = h, lower = a, upper = b, weights = wts, ...)
+  } else {
+    kde <- density(x, bw = h, from = a, to = b, weights = wts, ...)
   }
   
   f <- approxfun(kde$x, kde$y)
@@ -174,6 +242,7 @@ JonesEst <- function(x, w, reflect=FALSE, a=NULL, b=NULL, ...){
   upr <- max(kde$x)
   append(list(f=f, lower=lwr, upper=upr), kde)
 }
+
 
 # Calculate Holland 1995 niche summary metrics:
 # peak abundance, preferred environment, & tolerance
