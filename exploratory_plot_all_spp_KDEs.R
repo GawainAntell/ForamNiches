af97a4a6b8864b7f89cfcf08ab1d4671be893791
Gwen Antell <gwenantell@gmail.com>
2020-04-29 16:09:37 +0100

    Turn density-plotting code into a function

--- exploratory_plot_all_spp_KDEs.R
+++ exploratory_plot_all_spp_KDEs.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(cowplot)
 library(raster)
+library(testthat)
 
 day <- format(as.Date(date(), format="%a %b %d %H:%M:%S %Y"), format='%y-%m-%d')
 
@@ -8,68 +9,7 @@ df <- readRDS('Data/sampled_temp_ym_truncated_by_depth_20-04-05.rds')
 
 source('GSA_custom_ecospat_fcns.R')
 
-coldBins <- c(28, 268, 436)
-hotBins <- c(124, 332, 492)
-spp <- c('Globigerinoides ruber','Hirsutella scitula')
-
-# Figure 1 ----------------------------------------------------------------
-
-modCodes <- read.csv('Data/gcm_model_codes.csv', stringsAsFactors = FALSE)
-rAll <- list.files('Data/gcm_annual_mean/')
-rTemp <- rAll[grep('temp', rAll)]
-
-# set blue as low temp value
-colr <- rainbow(40)[c(30:1)]
-
-b1 <- 28
-b2 <- 124
-
-for (b in c(b1, b2)){
-  mod <- modCodes$age_1000ka==b
-  id <- modCodes$id[mod]
-  flPos <- grep(id, rTemp)
-  fl <- rTemp[flPos]
-  rNm <- paste0('Data/gcm_annual_mean/',fl)
-  r <- raster(rNm)
-  prj <- '+proj=laea' 
-  m <- projectRaster(r, crs=prj)
-  
-  mapNm <- paste0('Figs/globe_map_',b,'ka.pdf')
-  pdf(mapNm, width=20, height=20)
-  plot(m, axes=FALSE, box=FALSE, legend=FALSE, col=colr)
-  dev.off()
-  
-  # plot KDE of N pachyderma
-}
-
-
-
-# Niches for all species --------------------------------------------------
-
-plotL <- list()
-
-sppSurf <- df$temp_ym_surf$sp
-sampSurf <- df$temp_ym_surf$samp
-sppSub <- df$temp_ym_sub$sp
-sampSub <- df$temp_ym_sub$samp
-  
-  #for (d in 1:2){
-  #  sppDf <- list(sppSurf, sppSub)[[d]]
-  #  sampDf <- list(sampSurf, sampSub)[[d]]
-for (s in spp){
-  if (s=='Globigerinoides ruber'){
-    sppDf <- sppSurf
-    sampDf <- sampSurf
-  } 
-  if (s=='Hirsutella scitula'){
-    sppDf <- sppSub
-    sampDf <- sampSub
-  }
-  xmx <- max(sampDf$temp_ym)
-  xmn <- min(sampDf$temp_ym)
-  
-for (bin in c(hotBins, coldBins)){
-  
+plotDens <- function(bin, s, xmn, xmx, sppDf, sampDf){
   # calculate w
   sampBool <- sampDf$bin==bin
   sampDat <- sampDf$temp_ym[sampBool] 
@@ -77,7 +17,7 @@ for (bin in c(hotBins, coldBins)){
   # except that the ends turn down a bit more (more convexity).
   # Since it's more complicated and throws warnings, don't do it.
   densSamp <- density(sampDat, from=xmn, to=xmx) 
-#  densSamp <- density.reflected(sampSlc, lower=xmn, upper=xmx) 
+  #  densSamp <- density.reflected(sampSlc, lower=xmn, upper=xmx) 
   w <- approxfun(densSamp$x, densSamp$y)
   a <- min(densSamp$x)
   b <- max(densSamp$x)
@@ -102,36 +42,146 @@ for (bin in c(hotBins, coldBins)){
   noNa <- length(x) - length(spDat)
   plotDat$rugHack <- c(spDat, rep(NA, noNa))
   sPlot <- 
-    ggplot(data=plotDat, aes(x=x, y=kd)) +
+    ggplot(data=plotDat, aes(x = x, y = kd)) +
     theme_bw() +
     ggtitle(paste(bin, 'Ka')) +
-    geom_area(fill='grey80') +
+    geom_area(fill='lightblue1') +
     geom_line() +
-    geom_segment(x = pe, xend = pe, y = 0, yend = 1) +
+    geom_segment(x = pe, xend = pe, y = 0, yend = 1,
+                 colour = 'blue') +
     geom_segment(x = mean(spDat), xend = mean(spDat), 
-                 y = 0, yend = 1, colour = 'blue') +
+                 y = 0, yend = 1, colour = 'grey20') +
     geom_hline(yintercept = 0) +
-    geom_rug(aes(x = rugHack), sides = 'b', length=unit(0.045, "npc")) + 
+    geom_rug(aes(x = rugHack), colour = 'grey20',
+             sides = 'b', length = unit(0.045, "npc")) + 
     # ,outside = TRUE) +
     #   coord_cartesian(clip = "off") +
-    scale_x_continuous(expand=c(0, 0)) +
-    scale_y_continuous(limits=c(0,0.08)) +
+    scale_x_continuous(expand = c(0, 0)) +
+    scale_y_continuous(limits = c(0, 0.08)) +
     theme(axis.title.x = element_blank(),
           axis.title.y = element_blank(),
           plot.title = element_text(size = 9))
   sPlot <- sPlot +
-    geom_text(aes(fontface = 1),label = nlab, size = 3,
-              x = 3, y = 0.06) # 0.9 * max(plotDat$kd))
+    geom_text(aes(fontface = 'plain', family = 'mono'), 
+              label = nlab, size = 3.5, # 3.5mm = 10pt
+              x = 0.15 * max(plotDat$x), y = 0.07)
+  sPlot
+}
+
+# Figure 1 ----------------------------------------------------------------
+
+modCodes <- read.csv('Data/gcm_model_codes.csv', stringsAsFactors = FALSE)
+rAll <- list.files('Data/gcm_annual_mean/')
+rTemp <- rAll[grep('temp', rAll)]
+
+# set blue as low temp value
+colr <- rainbow(40)[c(30:1)]
+
+b1 <- 28
+b2 <- 124
+samp0m <- df$temp_ym_0m$samp
+spp0m <- df$temp_ym_0m$sp
+pachy <- 'Neogloboquadrina pachyderma'
+
+for (b in c(b1, b2)){
+  mod <- modCodes$age_1000ka==b
+  id <- modCodes$id[mod]
+  flPos <- grep(id, rTemp)
+  fl <- rTemp[flPos]
+  rNm <- paste0('Data/gcm_annual_mean/',fl)
+  r <- raster(rNm)
+  prj <- '+proj=laea' 
+  m <- projectRaster(r, crs=prj)
   
-  plotL <- append(plotL, list(sPlot))
+  mapNm <- paste0('Figs/globe_map_',b,'ka.pdf')
+  pdf(mapNm, width=20, height=20)
+  plot(m, axes=FALSE, box=FALSE, legend=FALSE, col=colr)
+  dev.off()
+  
+  # use N pachyderma as example to go with line drawing
+  sPlot <- plotDens(bin = b, s = pachy,
+           sampDf = samp0m, sppDf = spp0m,
+           xmx=max(samp0m$temp_ym),
+           xmn=min(samp0m$temp_ym)
+           )
+  sShrt <- strsplit(pachy, ' ')[[1]][2]
+  spPlotNm <- paste0('Figs/KDE-example-', sShrt, '-', b, 'ka_', day, '.pdf')
+  pdf(spPlotNm, width=2.5, height=2)
+  print(sPlot)
+  dev.off()
+}
+
+# Glacial/IG niches -------------------------------------------------------
+
+coldBins <- c(28, 268, 436)
+hotBins <- c(124, 332, 492)
+
+# pick 1 surface and 1 subsurface species as examples
+spp <- c('Globigerinoides ruber','Hirsutella scitula')
+
+sppSurf <- df$temp_ym_surf$sp
+sampSurf <- df$temp_ym_surf$samp
+sppSub <- df$temp_ym_sub$sp
+sampSub <- df$temp_ym_sub$samp
+
+plotL <- list()  
+
+  #for (d in 1:2){
+  #  sppDf <- list(sppSurf, sppSub)[[d]]
+  #  sampDf <- list(sampSurf, sampSub)[[d]]
+for (s in spp){
+  if (s=='Globigerinoides ruber'){
+    sppDf <- sppSurf
+    sampDf <- sampSurf
+  } 
+  if (s=='Hirsutella scitula'){
+    sppDf <- sppSub
+    sampDf <- sampSub
+  }
+  xmx <- max(sampDf$temp_ym)
+  xmn <- min(sampDf$temp_ym)
+  
+for (bin in c(hotBins, coldBins)){
+  
+  sbPlot <- plotDens(bin, s, xmn, xmx, sppDf, sampDf)
+  plotL <- append(plotL, list(sbPlot))
   
 }
 }
 
-# print page of plots
-pg <- plot_grid(plotlist=plotL, ncol=3)
+# combine plots onto 1 page with title for each species
+top <- plot_grid(plotlist = plotL[1:6], ncol = 3)
+topTtl <- ggdraw() + 
+  draw_label(
+    'Globigerinoides ruber',
+    fontface = 'bold.italic',
+    x = 0, hjust = 0
+  ) +
+  theme(
+    # add margin on the left of the drawing canvas,
+    # so title is aligned with left edge of first plot
+    plot.margin = margin(0, 0, 0, 7)
+  )
+btm <- plot_grid(plotlist = plotL[7:12], ncol = 3)
+btmTtl <- ggdraw() +
+  draw_label(
+    'Hirsutella scitula',
+    fontface = 'bold.italic',
+    x = 0, hjust = 0
+  ) +
+  theme(
+    # add margin on the left of the drawing canvas,
+    # so title is aligned with left edge of first plot
+    plot.margin = margin(0, 0, 0, 7)
+  )
+
 kdeNm <- paste0('Figs/KDE-examples-transform-RT_',day,'.pdf')
 pdf(kdeNm, width=6, height=6)
-print(pg)
+plot_grid(
+  topTtl, top, btmTtl, btm,
+  ncol = 1,
+  # rel_heights values control vertical title margins
+  rel_heights = c(0.1, 1)
+)
 dev.off()
 
