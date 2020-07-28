3863570655569ef9db4a0ad4a31b976960f529f7
Gwen Antell <gwenantell@gmail.com>
2020-07-28 09:42:15 +0100

    Use current paleotree package instead of unsupported paleoPhylo package

--- analysis_phylo_eco.R
+++ analysis_phylo_eco.R
@@ -1,47 +1,78 @@
 library(phytools)
-library(paleoPhylo)
+library(paleotree)
 library(ape)
 library(geiger)
 library(nlme)
 library(xtable)
 
 ss <- TRUE
+day <- as.Date(date(), format="%a %b %d %H:%M:%S %Y")
 
-# Data prep ---------------------------------------------------------------
+# Niche data --------------------------------------------------------------
 
-day <- format(as.Date(date(), format="%a %b %d %H:%M:%S %Y"), format='%y-%m-%d')
-
-# Foram niche data
-spAttr <- read.csv('Data/foram_spp_data_20-04-05.csv', stringsAsFactors=FALSE)
+spAttr <- read.csv('Data/foram-spp-data_2020-07-21.csv')
 if (ss){
-  df <- read.csv('Data/foram_niche_sumry_metrics_0m_20-04-05.csv', stringsAsFactors=FALSE)
+  # using RT bandwidth gives lambda = 0; SJ, 1
+ # df <- read.csv('Data/niche-sumry-metrics_nrd0_SS_2020-07-22.csv')
+  df <- read.csv('Data/niche-sumry-metrics_SJ-ste_SS_2020-07-24.csv')
 } else {
-  df <- read.csv('Data/foram_niche_sumry_metrics_20-04-05.csv', stringsAsFactors=FALSE)
+ #  df <- read.csv('Data/niche-sumry-metrics_nrd0_hab_2020-07-22.csv')
+  df <- read.csv('Data/niche-sumry-metrics_SJ-ste_hab_2020-07-24.csv')
 }
 ordr <- order(df$bin, decreasing = TRUE)
 df <- df[ordr,]
 bins <- unique(df$bin)
 spp <- unique(df$sp)
-nspp <- length(spp)
 
-# Phylogenetic tree
-trRaw <- read.csv('Data/Aze_et_al_2011_bifurcating_tree_data.csv', stringsAsFactors=FALSE)
-trRaw$Species.name <- gsub('Globigerinoides sacculifer', 'Trilobatus sacculifer', trRaw$Species.name)
-trRaw$Species.name <- gsub('Globigerinoides trilobus', 'Trilobatus trilobus', trRaw$Species.name)
-trPaleo <- with(trRaw, 
-                 as.paleoPhylo(Species.code, Ancestor.code, Start.date, End.date)
-)
-tr <- buildApe(trPaleo)
-sppCodes <- tr$tip.label
-rowOrdr <- match(sppCodes, trRaw$Species.code)
-tr$tip.label <- trRaw$Species.name[rowOrdr]
+# Phylogenetic tree -------------------------------------------------------
 
-# Summarise niche data for each species as the median among intervals, to plot
+data(macroperforateForam)
+
+# 2 species are assigned to a different genus in the phylogeny
+foramAMb$Species_name <- gsub('Globigerinoides_sacculifer', 'Trilobatus_sacculifer', 
+                              foramAMb$Species_name)
+foramAMb$Species_name <- gsub('Globigerinoides_trilobus', 'Trilobatus_trilobus', 
+                              foramAMb$Species_name)
+
+# from paleotree example: convert Aze et al's suppmat to paleotree-readable format
+createTaxaData <- function(table){
+  #reorder table by first appearance
+  timetable <- table[order(-as.numeric(table[,3])),]
+  ID <- 1:nrow(table)
+  anc <- sapply(table[,2], function(x){
+    if(!is.na(x)){
+      which(x == table[,1])
+    } else { NA }
+  })
+  stillAlive <- as.numeric(table[,4] == 0)
+  ages <- cbind(as.numeric(table[,3]),
+                as.numeric(table[,4]))
+  res <- cbind(ID,anc,ages,stillAlive,ID)
+  colnames(res) <- c('taxon.id','ancestor.id','orig.time',
+                     'ext.time','still.alive','looks.like')
+  rownames(res) <- table[,1]
+  return(res)
+}
+
+# warning: slow!
+taxaAMb <- createTaxaData(foramAMb)
+treeAMb <- taxa2phylo(taxaAMb)
+
+# drop anagenetic zero-length-terminal-edge ancestors
+cleanAMb <- dropZLB(treeAMb)
+
+# rename tips from alphanumeric codes to species names
+sppCodes <- cleanAMb$tip.label
+rowOrdr <- match(sppCodes, foramAMb$Species_code)
+cleanAMb$tip.label <- foramAMb$Species_name[rowOrdr]
+
+# Summarise H for each species --------------------------------------------
+
 spSmry <- function(s, dat){
   sBool <- dat$sp==s
   sDf <- dat[sBool,]
   numCol <- ! colnames(dat) %in% c('bin','sp')
-  out <- apply(sDf[,numCol], 2, median, na.rm=TRUE)
+  out <- apply(sDf[,numCol], 2, mean, na.rm=TRUE)
   out <- data.frame(t(out))
   out$nInt <- nrow(sDf)
   out$species <- s
@@ -51,7 +82,7 @@ spL <- lapply(spp, spSmry, dat=df)
 spDf <- do.call(rbind, spL)
 
 # Combine with species attribute data
-spDf <- merge(spDf, spAttr, by='species')
+spDf <- merge(spDf, spAttr, by = 'species')
 # set open ocean thermocline species as the reference level
 spDf$eco <- factor(spDf$eco, levels=c(3,1,2,4,5))
 ecoLbl <- c('Thermocline','Mix layer symbiotic','Mix layer heterotroph',
@@ -89,37 +120,35 @@ tbl$h <- sprintf('%.2f', round(tbl$h, 2))
 tbl$pe <- sprintf('%.1f', round(tbl$pe, 1))
 colnames(tbl) <- c('Species','Ecotype','Eco. ref.','Depth','Depth ref.','H','Optimum')
 tblx <- xtable(tbl, align=c(rep('l',6),'r','r'))
-tblNm <- paste0('Figs/species_attribute_table_0m_',day,'.tex')
+tblNm <- paste0('Figs/species-attribute-table_SS_',day,'.tex')
 if (ss){
-  print(tblx, file=tblNm, include.rownames=FALSE, 
+  print(tblx, file=tblNm, include.rownames = FALSE, 
         floating = FALSE, tabular.environment = 'longtable')
 }
 
 # Phylogenetic comparison -------------------------------------------------
+# http://www.phytools.org/Cordoba2017/ex/4/PGLS.html
 
 # Ensure uniform species name set in data and tree
-toss <- name.check(tr, spDf$species, data.names=spp)
+spDf$species <- gsub(' ', '_', spDf$species)
+toss <- name.check(cleanAMb, spDf$species, data.names = spDf$species)
 noPhy <- toss$data_not_tree
 if (length(noPhy) > 0){
   rows2toss <- spDf$species %in% noPhy
   spDf <- spDf[!rows2toss,]
   paste(paste(noPhy, collapse = ' '), 'removed')
-  spp <- setdiff(spp, noPhy)
 } 
-trTrim <- drop.tip(tr, toss$tree_not_data)
-name.check(trTrim, spDf$species, data.names=spp)
+trTrim <- drop.tip(cleanAMb, toss$tree_not_data)
+#  dropPaleoTip(treeAMb, toss$tree_not_data)
+name.check(trTrim, spDf$species, data.names = spDf$species)
+
+# TODO consider exporting as a supplemental figure
+plot(trTrim, main ='timetreeAMb', show.tip.label = T)
 
 # use nlme method - caper oddly drops 8 tree tips
 
-row.names(spDf) <- spDf$species
-spDf <- spDf[trTrim$tip.label,]
-
-bm <- corBrownian(1, trTrim)
-bmMod <- gls(h~eco, data=spDf, correlation=bm)
-summary(bmMod)
-
-pgl <- corPagel(1, trTrim)
-pglMod <- gls(h~eco, data=spDf, correlation = pgl)
+pgl <- corPagel(1, form = ~ species, trTrim)
+pglMod <- gls(h ~ eco, data = spDf, correlation = pgl)
 summary(pglMod)
 
 # ANOVA
@@ -127,20 +156,20 @@ summary(pglMod)
 h <- spDf$h
 eco <- spDf$eco
 names(eco) <- names(h) <- spDf$species
-aovRes <- aov.phylo(h ~ eco, trTrim, nsim=10000)
+aovRes <- aov.phylo(h ~ eco, trTrim, nsim = 10000)
 
 coefs <- summary(aovRes)$coefficients
-ecoEsts <- sprintf('%.3f', round(coefs[-1,'Estimate'], 3))
-ecoErr <- sprintf('%.3f', round(coefs[-1,'Std. Error'], 3))
-intrcpt <- coefs['(Intercept)',c('Estimate','Std. Error')]
+ecoEsts <- sprintf('%.3f', round(coefs[-1, 'Estimate'],  3))
+ecoErr <-  sprintf('%.3f', round(coefs[-1,'Std. Error'], 3))
+intrcpt <- coefs['(Intercept)', c('Estimate','Std. Error')]
 intrcptRnd <- sprintf('%.3f', round(intrcpt, 3))
 interceptRow <- cbind('Intercept', intrcptRnd[1], intrcptRnd[2])
-ecoRows <- cbind(Ecotype=ecoLbl, Coef=c(0, ecoEsts), SE=c('-', ecoErr))
+ecoRows <- cbind(Ecotype = ecoLbl, Coef = c(0, ecoEsts), SE = c('-', ecoErr))
 aovTbl <- rbind(interceptRow, ecoRows)
 aovX <- xtable(aovTbl)
 if (ss){
-  aovNm <- paste0('Figs/phylo_ANOVA_table_0m_',day,'.tex')
+  aovNm <- paste0('Figs/phylo-ANOVA-table_SS_',day,'.tex')
 } else {
-  aovNm <- paste0('Figs/phylo_ANOVA_table_hab_',day,'.tex')
+  aovNm <- paste0('Figs/phylo-ANOVA-table_hab_',day,'.tex')
 }
-print(aovX, file=aovNm, include.rownames=FALSE)
+print(aovX, file = aovNm, include.rownames = FALSE)
